e86c7097fc2e2d467af54c006df0b253f3de28c0
Yuri Braga <yuricancelabraga@gmail.com>
2019-04-09 07:52:08 -0300

    Components

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,9 +2,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtonText() {
+  return "Click on me!";
+}
+
 // Create a react component
 export default function App() {
-  return <div>Hi there!</div>;
+  const buttonText = "Click me!";
+  const style = { backgroundColor: "blue", color: "white" };
+  const labelText = "Enter name:";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={style}>{buttonText}</button>
+    </div>
+  );
 }
 
 // Take the react component and show it on the screen
