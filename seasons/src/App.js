141529d1ae1b6c78e0143d1cc06178a7578642fa
Yuri Braga <yuri@Yuri-Inspiron.localdomain>
2019-05-27 19:24:04 -0300

    67. adding some styling

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   constructor(props) {
@@ -28,7 +29,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
